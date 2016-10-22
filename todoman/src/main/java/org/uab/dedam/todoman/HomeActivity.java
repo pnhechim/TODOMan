05935dc16309c91daf8b6fbdbf7b67057114f8e7
Cristian Tanas <c.tanas@gmail.com>
2016-10-22 14:06:26 +0200

    Add a new activity for creating a new task.

--- todoman/src/main/java/org/uab/dedam/todoman/HomeActivity.java
+++ todoman/src/main/java/org/uab/dedam/todoman/HomeActivity.java
@@ -1,7 +1,10 @@
 package org.uab.dedam.todoman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button gotoSubactivity = (Button) findViewById(R.id.newTask);
+        gotoSubactivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, NewTaskActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
