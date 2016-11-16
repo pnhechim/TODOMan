2fe5c557410f83dc724e4c0a38c084b2c0e41cee
Nicolás Hechim <nicolashechim@gmail.com>
2016-11-16 20:33:37 +0100

    Clase acceso a hardware y sensores (agregamos la posibilidad de tomar una foto en una new task)

--- todoman/src/main/java/org/uab/dedam/todoman/NewTaskActivity.java
+++ todoman/src/main/java/org/uab/dedam/todoman/NewTaskActivity.java
@@ -5,22 +5,31 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
 
 public class NewTaskActivity extends AppCompatActivity
                              implements DatePickerFragment.OnDateSetCallBack{
 
     Button taskSetDate;
     Button save;
+    Button openCamera;
+    ImageView imagePreview;
     TextView taskEndDate;
-
     EditText taskTitle;
     EditText taskDescription;
     CheckBox taskCompleted;
@@ -35,6 +44,27 @@ public class NewTaskActivity extends AppCompatActivity
         taskTitle = (EditText) findViewById(R.id.taskTitle);
         taskDescription = (EditText) findViewById(R.id.taskDescription);
         taskCompleted = (CheckBox) findViewById(R.id.taskComplete);
+        imagePreview = (ImageView) findViewById(R.id.imageView);
+        openCamera = (Button) findViewById(R.id.openCamera);
+
+        openCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                        File photoFile = File.createTempFile(
+                                "MyImageName",
+                                "JPEG",
+                                getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+                        );
+                        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoFile.toURI());
+                        startActivityForResult(cameraIntent, 1);
+
+                } catch (IOException e) {
+                    Log.e("","");
+                }
+            }
+        });
 
         taskSetDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,6 +118,14 @@ public class NewTaskActivity extends AppCompatActivity
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 1 && resultCode == RESULT_OK) {
+            Bitmap thumbail = (Bitmap) data.getExtras().get("data") ;
+            imagePreview.setImageBitmap(thumbail);
+        }
     }
 
     @Override
